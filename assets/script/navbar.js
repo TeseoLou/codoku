8ed9fb04dee9a766b34e4aadaa698ba35b505eac
Teseo Nicholson <teseonicholson@gmail.com>
2025-06-02 18:56:22 +0100

    Add audio file for page turn in the object

---
+++ assets/script/navbar.js
@@ -0,0 +1,7 @@
+// Navigation sound effects
+// Object to store and control navigation sound effects
+// Reference: https://stackoverflow.com/questions/40100433
+const navSounds = {
+    // Retrieve the sound object matching the given name from the themeToggleSounds object
+    page: new Audio("assets/sounds/page.mp3")
+};
