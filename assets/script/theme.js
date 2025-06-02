449a5030fe686919c8b2f511651d180d95087d02
Teseo Nicholson <teseonicholson@gmail.com>
2025-06-02 16:03:13 +0100

    Create themeToggleSounds object for audio feedback on themes switch in theme.js

---
+++ assets/script/theme.js
@@ -0,0 +1,22 @@
+// Theme toggle sound effects
+// Object to store and control theme toggle sound effects
+// Reference: https://stackoverflow.com/questions/40100433
+const themeToggleSounds = {
+    // The 'tweet' sound for light mode
+    tweet: new Audio("assets/sounds/tweet.mp3"),
+    // The 'hoot' sound for dark mode
+    hoot: new Audio("assets/sounds/hoot.mp3"),
+    // Method that plays a sound effect by name 
+    // Reference: https://developer.mozilla.org/en-US/docs/Web/API/HTMLAudioElement
+    play(name) {
+        // Retrieve the sound object matching the given name from the themeToggleSounds object
+        const sound = this[name];
+        // Check if a valid sound was found for the given name
+        if (sound) {
+            // Restart sound
+            sound.currentTime = 0;
+            // Play the sound
+            sound.play();
+        }
+    }
+};
