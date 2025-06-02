bfae99e8ead053dd3b7077f98cd81ffee98ac3aa
Teseo Nicholson <teseonicholson@gmail.com>
2025-06-02 19:32:23 +0100

    Inside event listener write if statement which checks if the click is not inside the navbar, not on the hamburger and that the bootstrap navbar is opened

--- assets/script/navbar.js
+++ assets/script/navbar.js
@@ -30,11 +30,15 @@ function setupOutsideNavbarCollapse() {
         // Return early if navbar content element is not found in the DOM
         // Reference: https://www.sitepoint.com/jquery-check-element-exists/
         if (!navbarCollapse.length) return;
-        // Check if the click happened inside the navbar content using jQuery
+        // Check if the click happened inside the navbar content 
         // Reference: https://stackoverflow.com/questions/62375324
         const isClickInsideNavbar = $(event.target).closest("#navbar-content").length > 0;
-        // Check if the click target is the navbar toggler or inside it using jQuery
+        // Check if the click target is the navbar toggler or inside 
         // Reference: https://stackoverflow.com/questions/46736823
         const isNavbarToggler = $(event.target).is(".navbar-toggler") || $(event.target).closest(".navbar-toggler").length > 0;
+        // If the click is outside both the navbar and toggler, and the navbar is expanded
+        if (!isClickInsideNavbar && !isNavbarToggler && $navbarCollapse.hasClass("show")) {
+            
+        }
     });
 }
