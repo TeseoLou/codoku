c3b10b2d218be9f41a9542686e2c3960aa260d70
Teseo Nicholson <teseonicholson@gmail.com>
2025-06-02 20:45:23 +0100

    Inside setupStartButton function use document.getElementById to find the enter button and write an if statement which will exit the function early if it isn't found

--- assets/script/setup-modal.js
+++ assets/script/setup-modal.js
@@ -2,5 +2,10 @@
  * Close the setup modal when the Enter button is clicked
  */
 function setupStartButton() {
-    
+    // Attach a click event handler to the Start button
+    // Reference: https://stackoverflow.com/questions/61793029
+    const startButton = document.getElementById("start-button");
+    if (!startButton) {
+        return;
+    }
 }
