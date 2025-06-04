e4917084ad0d5e240fecf47300d9c456bff53218
Teseo Nicholson <teseonicholson@gmail.com>
2025-06-04 11:19:31 +0100

    Create an empty for loop inside renderEmptyGrid function which will be used to create the rows of the grid

--- assets/script/game.js
+++ assets/script/game.js
@@ -7,6 +7,11 @@ function renderEmptyGrid() {
     // Clear any existing content
     // Reference: https://stackoverflow.com/questions/1701973
     gridContainer.empty();
+    // Loop through each row index (0 to 8) to build 9 rows for the grid
+    // Reference: https://teamtreehouse.com/community/create-an-interactive-grid-with-jquery?
+    for (let row = 0; row < 9; row++) {
+
+    }
 }
 
 // Call function - Render a blank 9x9 Sudoku grid
