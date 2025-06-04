55e9b266484f0be1be802e4959bfd9961c2c2282
Teseo Nicholson <teseonicholson@gmail.com>
2025-06-04 11:17:28 +0100

    Inside renderEmptyGrid function target the grid element in index.html and then use .empty() method to clear any content from it to avoid grid duplication

--- assets/script/game.js
+++ assets/script/game.js
@@ -2,7 +2,11 @@
  * Render a blank 9x9 Sudoku grid
  */
 function renderEmptyGrid() {
-    
+    // Get the grid container
+    const gridContainer = $('#grid');
+    // Clear any existing content
+    // Reference: https://stackoverflow.com/questions/1701973
+    gridContainer.empty();
 }
 
 // Call function - Render a blank 9x9 Sudoku grid
