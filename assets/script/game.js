d7bae31bc6f3d34effeb384b90cc05f5708442a1
Teseo Nicholson <teseonicholson@gmail.com>
2025-06-04 11:23:17 +0100

    Inside the for loop, create 9 rowDivs with flexbox layout for each row and add each new row to the preceeding one using .append()

--- assets/script/game.js
+++ assets/script/game.js
@@ -1,6 +1,7 @@
 /**
  * Render a blank 9x9 Sudoku grid
  */
+// Reference: https://www.geeksforgeeks.org/create-a-sudoku-puzzle-using-html-css-javascript/
 function renderEmptyGrid() {
     // Get the grid container
     const gridContainer = $('#grid');
@@ -10,7 +11,10 @@ function renderEmptyGrid() {
     // Loop through each row index (0 to 8) to build 9 rows for the grid
     // Reference: https://teamtreehouse.com/community/create-an-interactive-grid-with-jquery?
     for (let row = 0; row < 9; row++) {
-
+        // Create a row div with Bootstrap flex class
+        const rowDiv = $('<div>').addClass('d-flex');
+        // Add row to grid
+        gridContainer.append(rowDiv); 
     }
 }
 
