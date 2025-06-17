990952140fc1e5c48b42c27857a2fb7b48ea753c
Teseo Nicholson <teseonicholson@gmail.com>
2025-06-17 13:22:45 +0100

    Delete unneccessary comments in setup-modal.js

--- assets/script/setup-modal.js
+++ assets/script/setup-modal.js
@@ -2,24 +2,19 @@
  * Close the setup modal when the Enter button is clicked
  */
 function setupStartButton() {
-    // Attach a click event handler to the Start button
     // Reference: https://stackoverflow.com/questions/61793029
     const startButton = document.getElementById("start-button");
     if (!startButton) {
         return;
     };
     startButton.addEventListener("click", function () {
-        // Get the setup modal element
         const setupModal = $("#setup-modal");
-        // Get the Bootstrap modal instance using jQuery wrapper for the setup modal
         // Reference: https://getbootstrap.com/docs/5.3/components/modal/#methods
         const setupModalBootstrap = bootstrap.Modal.getInstance(setupModal[0]);
-        // If the modal instance exists hide the modal
         if (setupModalBootstrap) {
             // Reference: https://getbootstrap.com/docs/5.0/components/modal/#hide
             setupModalBootstrap.hide();
         };
-        // Call function - Start a game by providing a fresh board, resetting stats, and resetting the timer and game stats
         startNewGame();
     });
 }
